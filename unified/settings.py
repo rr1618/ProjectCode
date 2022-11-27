3adaf0d5e39e851dd904fe64758bade8b5b77d6a
Subham Kumar <shubham.kumar.ggps@gmail.com>
2022-11-27 22:00:23 +0530

    deployment setup 2

--- unified/settings.py
+++ unified/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-2uoa9%8%1j28y%ap#!r2sivxdz6ap75ylghl*mu#jus7(@94m+
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://unifiedapp.herokuapp.com/'
+    'unifiedapp.herokuapp.com'
 ]
 
 
